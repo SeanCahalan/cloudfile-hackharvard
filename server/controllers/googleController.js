be90e8cf887d86b75e8da6161c8a3b9355475733
Pete Wright <pewright77@gmail.com>
2018-10-20 12:58:00 -0400

    Return data structured as fs
    fs for filesystem

--- server/controllers/googleController.js
+++ server/controllers/googleController.js
@@ -4,6 +4,27 @@ const { google } = require("googleapis");
 const oAuth2Client = require("../config/google");
 const TOKEN_PATH = "../config/token.json";
 
+let compareFiles = function(file1, file2) {
+  return file1.parents.length - file2.parents.length;
+};
+
+let addFileToStructure = function(fs, file) {
+  let path = fs;
+  let parents = file.parents
+
+  // get the part of the json we want to put the new file in
+  for (i=0; i<parents.length; i++)
+    path = path[parents[i]];
+
+  path[file.id] = {
+    name: file.name,
+    type: file.mimeType.split('.').pop(),
+    size: file.size,
+    lastModified: file.modifiedTime,
+    service: 'google'
+  };
+};
+
 module.exports = {
   fetchDrive: function(req, res, next) {
     fs.readFile(TOKEN_PATH, (err, token) => {
@@ -15,16 +36,15 @@ module.exports = {
         },
         (err, res) => {
           if (err) return console.log("The API returned an error: " + err);
-          const files = res.data.files;
-          let bibbity = [];
+          let files = res.data.files;
+          // sort files before putting them in the json
+          files = files.sort(compareFiles);
+          let fileStructure = {};
           for (let i = 0; i < files.length; i++) {
             let entry = files[i];
-            bibbity.push({
-              fileid: entry.id,
-              name: entry.name,
-              service: "google"
-            });
+            addFileToStructure(fileStructure, entry);
           }
+          res.status(200).send(fileStructure);
         }
       );
     });
