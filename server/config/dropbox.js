501bdb0245e9551deef1271a598d028857d9ddca
Akin <akinkunmishonibare@gmail.com>
2018-10-20 11:43:06 -0400

    initialized google drive with list and delete functions

--- server/config/dropbox.js
+++ server/config/dropbox.js
@@ -1,5 +1,8 @@
-const config = require('./main');
-const fetch = require('isomorphic-fetch');
-const Dropbox = require('dropbox').Dropbox;
+const config = require("./main");
+const fetch = require("isomorphic-fetch");
+const Dropbox = require("dropbox").Dropbox;
 // TODO: use accessToken from user rather than config
-module.exports = new Dropbox({ accessToken: config.dropbox.access_token, fetch: fetch });
+module.exports = new Dropbox({
+  accessToken: config.dropbox.access_token,
+  fetch: fetch
+});
