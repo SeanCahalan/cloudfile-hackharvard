24dbce6d6634f63679493831260992e6cb54f339
Pete Wright <pewright77@gmail.com>
2018-10-20 12:58:00 -0400

    Return data structured as fs
    fs for filesystem

--- server/controllers/googleController.js
+++ server/controllers/googleController.js
@@ -1,6 +1,27 @@
 "use strict";
 let google;
 
+let compareFiles = function(file1, file2) {
+  return file1.parents.length - file2.parents.length;
+};
+
+let addFileToStructure = function(fs, file) {
+  let path = fs;
+  let parents = file.parents
+
+  // get the part of the json we want to put the new file in
+  for (i=0; i<parents.length; i++)
+    path = path[parents[i]];
+
+  path[file.id] = {
+    name: file.name,
+    type: file.mimeType.split('.').pop(),
+    size: file.size,
+    lastModified: file.modifiedTime,
+    service: 'google'
+  };
+};
+
 module.exports = {
 
   middleware: function(req, res, next) {
@@ -14,25 +35,27 @@ module.exports = {
   },
 
   fetch: function(req, res, next) {
-
-    return google.files.list({
-      fields: "nextPageToken, files(id, name, parents, mimeType)"
-    })
-    .then(result => {
-      const files = result.data.files;
-      let bibbity = [];
-      for (let i = 0; i < files.length; i++) {
-        let entry = files[i];
-        bibbity.push({
-          fileid: entry.id,
-          name: entry.name,
-          service: "google"
-        });
-      }
-
-      return res.status(200).send(bibbity);
-    })
-    .catch(err => next(err));
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      const auth = oAuth2Client.setCredentials(JSON.parse(token));
+      const drive = google.drive({ version: "v3", auth });
+      drive.files.list(
+        {
+          fields: "nextPageToken, files(id, name, parents, mimeType)"
+        },
+        (err, res) => {
+          if (err) return console.log("The API returned an error: " + err);
+          let files = res.data.files;
+          // sort files before putting them in the json
+          files = files.sort(compareFiles);
+          let fileStructure = {};
+          for (let i = 0; i < files.length; i++) {
+            let entry = files[i];
+            addFileToStructure(fileStructure, entry);
+          }
+          res.status(200).send(fileStructure);
+        }
+      );
+    });
   },
 
   delete: function(req, res, next) {
