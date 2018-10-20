c1eeb72ae0f3d6f844a6dcb1e6aeeeaefbf718c2
Akin <akinkunmishonibare@gmail.com>
2018-10-20 17:33:30 -0400

    added create folder functionality

--- server/controllers/googleController.js
+++ server/controllers/googleController.js
@@ -2,44 +2,57 @@
 let google;
 
 module.exports = {
-
   middleware: function(req, res, next) {
     const user = req.user;
-    if (!user.google.access_token)
-      return next('User has not added google')
-    if (!google)
-      google = require('../config/google')(user.google)
+    if (!user.google.access_token) return next("User has not added google");
+    if (!google) google = require("../config/google")(user.google);
 
     return next();
   },
 
   fetch: function(req, res, next) {
+    return google.files
+      .list({
+        fields: "nextPageToken, files(id, name, parents, mimeType)"
+      })
+      .then(result => {
+        const files = result.data.files;
+        let bibbity = [];
+        for (let i = 0; i < files.length; i++) {
+          let entry = files[i];
+          bibbity.push({
+            fileid: entry.id,
+            name: entry.name,
+            service: "google"
+          });
+        }
 
-    return google.files.list({
-      fields: "nextPageToken, files(id, name, parents, mimeType)"
-    })
-    .then(result => {
-      const files = result.data.files;
-      let bibbity = [];
-      for (let i = 0; i < files.length; i++) {
-        let entry = files[i];
-        bibbity.push({
-          fileid: entry.id,
-          name: entry.name,
-          service: "google"
-        });
-      }
-
-      return res.status(200).send(bibbity);
-    })
-    .catch(err => next(err));
+        return res.status(200).send(bibbity);
+      })
+      .catch(err => next(err));
   },
 
   delete: function(req, res, next) {
     const fileId = req.body.file;
 
-    return google.files.delete({ fileId })
+    return google.files
+      .delete({ fileId })
       .then(result => res.status(200).send(result.data))
-      .catch(err => next(err))
+      .catch(err => next(err));
+  },
+
+  createFolder: function(req, res, next) {
+    const folderName = req.body.folderName;
+    var fileMetadata = {
+      name: folderName,
+      mimeType: "application/vnd.google-apps.folder"
+    };
+    return drive.files
+      .create({
+        resource: fileMetadata,
+        fields: "id"
+      })
+      .then(result => res.status(200).send(result.data))
+      .catch(err => next(err));
   }
 };
