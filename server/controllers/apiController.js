5b6669f32c2372f7063a2f4c995b251e8ed8be35
Francesco Virga <francescod.virga@gmail.com>
2018-10-20 00:35:53 -0400

    Dropbox download

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -5,7 +5,7 @@
 
 module.exports = {
 
-  dropbox: function(req, res, next) {
+  fetchDropbox: function(req, res, next) {
     return dropbox.filesListFolder({path: ''})
       .then(results => res.status(200).send(results))
       .catch(err => next(err));
@@ -16,7 +16,7 @@ module.exports = {
   // path example: "/files/images"
   uploadDropbox: function(req, res, next) {
     if (!req.files.file[0])
-      throw new Error('No image provided');
+      return next(new Error('No image provided'));
     if (!req.body.path)
       return next(new Error('No path provided'))
 
@@ -24,6 +24,28 @@ module.exports = {
     const path = req.body.path
     return dropbox.filesUpload({ path: `${path}/${file.originalname}`, contents: file.buffer })
       .then(result => res.status(201).send(result))
+      .catch(err => next(err));
+  },
+
+  // this is some whack fuckery but i think it works
+  downloadDropbox: function(req, res, next) {
+    if (!req.body.path)
+      return next(new Error('No path provided'))
+    const path = req.body.path;
+    return dropbox.filesDownload({ path: path })
+      .then(results => {
+        const data = results.fileBinary
+        const fileName = results.name;
+        const fileSplit = fileName.split('.')
+        const extension = fileSplit[fileSplit.length-1];
+
+        res.writeHead(200, {
+          'Content-Type': `application/${extension}`,
+          'Content-Disposition': `attachment; filename=${fileName}`,
+          'Content-Length': data.length
+        });
+        res.end(data);
+      })
       .catch(err => next(err));
   }
 };
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -7,8 +7,9 @@ const upload = multer()
 // PUT ALL API ROUTE DEFINITIONS HERE
 
 const apiRouter = express.Router();
-apiRouter.get('/dropbox/fetch', apiController.dropbox);
+apiRouter.get('/dropbox/fetch', apiController.fetchDropbox);
 apiRouter.post('/dropbox/upload', upload.fields([{ name: 'file', maxCount: 1 },
   { name: 'path', maxCount: 1 }]), apiController.uploadDropbox);
+apiRouter.post('/dropbox/download', apiController.downloadDropbox);
 
 module.exports = apiRouter;
