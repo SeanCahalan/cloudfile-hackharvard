00985a71813bd388492e4a5c256fcf9c23ddf95f
Akin <akinkunmishonibare@gmail.com>
2018-10-20 11:43:06 -0400

    initialized google drive with list and delete functions

--- server/config/google.js
+++ server/config/google.js
@@ -1,1 +1,10 @@
 // add google sdk config, then module.exports = google
+const config = require("./main");
+const { google } = require("googleapis");
+
+// TODO: use accessToken from user rather than config
+module.exports = new google.auth.OAuth2({
+  client_id: config.google.client_id,
+  client_secret: config.google.client_secret,
+  redirect_uris: config.google.redirect_uris[0]
+});
--- server/config/main.js
+++ server/config/main.js
@@ -5,6 +5,17 @@ module.exports = {
     app_key: process.env.DROPBOX_APP_KEY,
     app_secret: process.env.DROPBOX_APP_SECRET
   },
+  google: {
+    client_id:
+      "219082002868-jn4q1i7cfqlf77qe8ldf46tkfg23hooh.apps.googleusercontent.com",
+    project_id: "hackharvard-220001",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://www.googleapis.com/oauth2/v3/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_secret: "EmTMVLtGPd-LDh1mhIXoE8dw",
+    redirect_uris: ["https://cloudfile.localtunnel.me/oauth"],
+    javascript_origins: ["https://cloudfile.localtunnel.me"]
+  },
   database: process.env.MONGODB_URI,
   node_env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000
