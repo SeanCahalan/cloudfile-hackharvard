ccabb1f90c4b668bc11b1d3a5995e67a5488c44f
Francesco Virga <francescod.virga@gmail.com>
2018-10-20 13:04:27 -0400

    FUCK google

--- server/controllers/googleController.js
+++ server/controllers/googleController.js
@@ -15,27 +15,20 @@ module.exports = {
 
   fetch: function(req, res, next) {
 
-    return google.files.list(
-      {
-        fields: "nextPageToken, files(id, name, parents, mimeType)"
-      },
-      (err, res) => {
-        if (err) return console.log("The API returned an error: " + err);
-        const files = res.data.files;
-        let bibbity = [];
-        for (let i = 0; i < files.length; i++) {
-          let entry = files[i];
-          bibbity.push({
-            fileid: entry.id,
-            name: entry.name,
-            service: "google"
-          });
-        }
+    return google.files.list({
+      fields: "nextPageToken, files(id, name, parents, mimeType)"
+    })
+    .then(res => {
+      const files = res.data.files;
+      let bibbity = [];
+      for (let i = 0; i < files.length; i++) {
+        let entry = files[i];
+        bibbity.push({
+          fileid: entry.id,
+          name: entry.name,
+          service: "google"
+        });
       }
-    )
-    .then(result => {
-      console.log(result)
-      return res.status(200).send(result)
     })
     .catch(err => next(err));
   },
