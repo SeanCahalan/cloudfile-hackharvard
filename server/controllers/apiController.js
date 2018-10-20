8f46dfcbd8ba0c72ab611b23b855cabd73ebeda2
Francesco Virga <francescod.virga@gmail.com>
2018-10-19 23:20:34 -0400

    Dropbox file upload

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -8,11 +8,21 @@ module.exports = {
     console.log(req.user)
     return res.status(200).send(req.user);
   },
-  
+
   dropbox: function(req, res, next) {
     return dropbox.filesListFolder({path: ''})
       .then(results => res.status(200).send(results))
       .catch(err => next(err));
 
+  },
+
+  uploadDropbox: function(req, res, next) {
+    if (!req.file)
+      throw new NoDataError('No image provided');
+    console.log(req.file)
+
+    return dropbox.filesUpload({ path: `/${req.file.originalname}`, contents: req.file.buffer })
+      .then(result => res.status(201).send(result))
+      .catch(err => next(err));
   }
 };
