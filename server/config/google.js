25220c04703917e377ece30498d349cc69d7b68b
Francesco Virga <francescod.virga@gmail.com>
2018-10-20 12:57:49 -0400

    Config google

--- server/config/google.js
+++ server/config/google.js
@@ -1,14 +1,12 @@
-// add google sdk config, then module.exports = google
 const config = require("./main");
 const { google } = require("googleapis");
 
-// TODO: use accessToken from user rather than config
 module.exports = function(googleConfig) {
-  var oAuth2Client = new google.auth.OAuth2({
-    client_id: config.google.client_id,
-    client_secret: config.google.client_secret,
-    redirect_uris: config.google.redirect_uris[0]
-  });
+  var oAuth2Client = new google.auth.OAuth2(
+    config.google.client_id,
+    config.google.client_secret,
+    config.google.redirect_uris[0]
+  );
 
   oAuth2Client.setCredentials(googleConfig)
   return google.drive({ version: "v3", oAuth2Client });
--- server/controllers/googleController.js
+++ server/controllers/googleController.js
@@ -5,33 +5,39 @@ module.exports = {
 
   middleware: function(req, res, next) {
     const user = req.user;
-    console.log(user)
     if (!user.google.access_token)
       return next('User has not added google')
     if (!google)
       google = require('../config/google')(user.google)
+
     return next();
   },
 
   fetch: function(req, res, next) {
-      google.files.list(
-        {
-          fields: "nextPageToken, files(id, name, parents, mimeType)"
-        },
-        (err, res) => {
-          if (err) return console.log("The API returned an error: " + err);
-          const files = res.data.files;
-          let bibbity = [];
-          for (let i = 0; i < files.length; i++) {
-            let entry = files[i];
-            bibbity.push({
-              fileid: entry.id,
-              name: entry.name,
-              service: "google"
-            });
-          }
+
+    return google.files.list(
+      {
+        fields: "nextPageToken, files(id, name, parents, mimeType)"
+      },
+      (err, res) => {
+        if (err) return console.log("The API returned an error: " + err);
+        const files = res.data.files;
+        let bibbity = [];
+        for (let i = 0; i < files.length; i++) {
+          let entry = files[i];
+          bibbity.push({
+            fileid: entry.id,
+            name: entry.name,
+            service: "google"
+          });
         }
-      );
+      }
+    )
+    .then(result => {
+      console.log(result)
+      return res.status(200).send(result)
+    })
+    .catch(err => next(err));
   },
 
   deleteFile: function(req, res, next) {
